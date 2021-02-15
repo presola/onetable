eb5785967d6d94dfe7ecc69a625dd8996829005a
presola <53655433+presola@users.noreply.github.com>
2021-02-14 21:55:53 -0400

    Update dev.py

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -2,9 +2,14 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+import dj_database_url
 
 from .config import *
 DATABASES = local_database
+default_object = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+if 'ENGINE' in default_object:
+    DATABASES['default'] = default_object
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'p6_izlna_zfupnk57int)z2cew$qs7=q(hkm)f^1xhc%5pyurr'
